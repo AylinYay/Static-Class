172e106ef7f3db8038325660898300f0c5e7102d
Aylin Yay <aylinyay@outlook.com>
2023-03-28 20:20:07 +0300

    Add project files.

---
+++ Static-Class/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çalışan Sayısı : {0}", Calisan.CalisanSayisi);
+
+            Calisan calisan = new Calisan("Ayşe","Yılmaz","İK");
+            Console.WriteLine("Çalışan Sayısı : {0}", Calisan.CalisanSayisi);
+
+
+            Console.WriteLine("Toplama İşlemi Sonucu : {0}",Islemler.Topla(100, 200));
+            Console.WriteLine("Çıkarma İşlemi Sonucu : {0}", Islemler.Cikar(400, 50));
+
+
+        }
+    }
+
+    class Calisan
+    {
+        private static int calisanSayisi;
+
+        public static int CalisanSayisi { get => calisanSayisi;  }
+
+        private string Isim;
+        private string Soyisim;
+        private string Departman;
+
+        static Calisan()
+        {
+            calisanSayisi = 0;
+        }
+
+        public Calisan(string ısim, string soyisim, string departman)
+        {
+            Isim = ısim;
+            Soyisim = soyisim;
+            Departman = departman;
+            calisanSayisi ++;
+        }
+    }
+
+    static class Islemler
+    {
+        public static long Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public static long Cikar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+    }
+}
